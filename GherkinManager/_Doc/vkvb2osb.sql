ae12e3a031cd0bd9ba46ab6a165af6872204f813
Laszlo Agotai <Laszlo.AGOTAI@xapt.com>
2021-06-27 20:08:31 +0200

    Added install documentation and SQL script

---
+++ GherkinManager/_Doc/vkvb2osb.sql
@@ -0,0 +1,40 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Feature] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Owner] nvarchar(max) NOT NULL,
+    [Content] nvarchar(max) NOT NULL,
+    [ProjectId] int NOT NULL,
+    CONSTRAINT [PK_Feature] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Project] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Owner] nvarchar(max) NULL,
+    [StartDate] datetime2 NOT NULL,
+    [EndDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_Project] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210627133341_CreateProjectAndFeature', N'5.0.7');
+GO
+
+COMMIT;
+GO
+
